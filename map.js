e1790aefb4db1718d0b5f1e31c4652f7d52a9124
JhanviShah1 <jhanvi.213@gmail.com>
2021-07-28 10:48:41 -0400

    our own map func

--- map.js
+++ map.js
@@ -24,6 +24,14 @@ const assertArraysEqual = function(array1, array2) {
     console.log(`Assertion failed: ${array1}!==${array2}`);
   }
 };
-assertArraysEqual([1,2,3],[1,2,3]);
-assertArraysEqual([1,2,3],[1,2,4]);
-assertArraysEqual(['1','hi','banana'],[1,2,4]);
+const words = ["ground", "control", "to", "major", "tom"];
+const map = function(array, callback) {
+  const results = [];
+  for (let item of array) {
+    results.push(callback(item));
+  }
+  return results;
+};
+const results1 = map(words,word=> word[0]);
+console.log(results1);
+assertArraysEqual(map(words,word=> word[0]),['g','c','t','m','t']);
